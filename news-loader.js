02c50f6122b86637933f5d286da99b152b4c64fc
Kokochuna CLGM <130992006+ClubGames@users.noreply.github.com>
2026-06-30 12:29:10 +0700

    Add files via upload

--- news-loader.js
+++ news-loader.js
@@ -3,23 +3,23 @@
     container.innerHTML = `
         <div class="news-card news-with-image featured" onclick="navigateToLatestNews()" style="cursor: pointer;">
             <div class="click-indicator">Нажмите для перехода</div>
-            <h3>🎮 Обновление SoI 0.02.0!</h3>
-            <span class="news-date">5 апреля 2026</span>
+            <h3>🎮 Обновление SoI 0.02.1!</h3>
+            <span class="news-date">30 июня 2026</span>
 
-            <div class="news-image-container">
-                <img src="assets/images/SoI_icon.png"
-                     alt="Survival On Islands 0.02.0"
-                     class="news-image">
-            </div>
+                <div class="news-image-container">
+                    <img src="assets/images/SoI_icon.png"
+                         alt="Survival On Islands 0.02.1"
+                         class="news-image">
+                </div>
 
-            <p>Глобальное обновление: процедурная генерация, фермерство и консоль (F1) уже в игре!</p>
-            <p>---> <a href="news.html#news-SoI-0020">Подробнее об обновлении</a></p>
+            <p>Исправлены баги с системой крафта, сохранения мира и процедурной генерации.</p>
+            <p>---> <a href="news.html#news-SoI-0021">Подробнее об обновлении</a></p>
         </div>
     `;
 }
 
 function navigateToLatestNews() {
-    window.location.href = 'news.html#news-SoI-0020';
+    window.location.href = 'news.html#news-SoI-0021';
 }
 
 function showErrorMessage(message) {
